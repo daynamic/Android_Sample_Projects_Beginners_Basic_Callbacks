5a7e9107c1d907d0c202f14cb49481342f4d36e6
Sharma <asharm93@ms.ds.uhc.com>
2017-02-26 20:47:00 +0530

    SMS Demo Using Code with compatibility for Marshmallow.

---
+++ SMSDemoUsingBuiltinIntent/app/src/main/java/com/test/asharm93/smsdemousingbuiltinintent/MainActivity.java
@@ -0,0 +1,127 @@
+package com.test.asharm93.smsdemousingbuiltinintent;
+
+import android.Manifest;
+import android.annotation.TargetApi;
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Build;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    Button send;
+    Context context=MainActivity.this;
+    public static final int MY_PERMISSIONS_REQUEST_WRITE_MESSAGE = 123;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        send=(Button)findViewById(R.id.compose_sms);
+        send.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                boolean result = checkPermission();
+                if (result) {
+                    sendsms();
+                }
+
+            }
+        });
+
+    }
+    @TargetApi(Build.VERSION_CODES.JELLY_BEAN)
+    public boolean checkPermission()
+    {
+        int currentAPIVersion = Build.VERSION.SDK_INT;
+        if(currentAPIVersion>=android.os.Build.VERSION_CODES.M)
+        {
+            if (ContextCompat.checkSelfPermission(context, Manifest.permission.SEND_SMS) != PackageManager.PERMISSION_GRANTED) {
+                if (ActivityCompat.shouldShowRequestPermissionRationale((Activity) context, Manifest.permission.SEND_SMS)) {
+                    AlertDialog.Builder alertBuilder = new AlertDialog.Builder(context);
+                    alertBuilder.setCancelable(true);
+                    alertBuilder.setTitle("Permission necessary");
+                    alertBuilder.setMessage("Message permission is necessary to send message!!!");
+                    alertBuilder.setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
+                        @TargetApi(Build.VERSION_CODES.JELLY_BEAN)
+                        public void onClick(DialogInterface dialog, int which) {
+                            ActivityCompat.requestPermissions((Activity)context, new String[]{Manifest.permission.SEND_SMS}, MY_PERMISSIONS_REQUEST_WRITE_MESSAGE);
+                        }
+                    });
+                    AlertDialog alert = alertBuilder.create();
+                    alert.show();
+                } else {
+                    ActivityCompat.requestPermissions((Activity)context, new String[]{Manifest.permission.CALL_PHONE}, MY_PERMISSIONS_REQUEST_WRITE_MESSAGE);
+                }
+                return false;
+            } else {
+                return true;
+            }
+        } else {
+            return true;
+        }
+    }
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        switch (requestCode) {
+            case MY_PERMISSIONS_REQUEST_WRITE_MESSAGE:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    sendsms();
+                } else {
+//code for deny
+                }
+                break;
+        }
+    }
+    private void sendsms(){
+
+        Intent sms=new Intent(Intent.ACTION_VIEW);
+        sms.setData(Uri.parse("smsto:"));
+        sms.setType("vnd.android-dir/mms-sms");
+
+        sms.putExtra("address", new String("8412866840"));
+        sms.putExtra("sms_body","kaisa hai aey");
+
+        try{
+            startActivity(sms);
+            finish();
+        }
+        catch(android.content.ActivityNotFoundException ex){
+            Toast.makeText(MainActivity.this, "SMS failed, pleae try again ;later", Toast.LENGTH_SHORT).show();
+        }
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
